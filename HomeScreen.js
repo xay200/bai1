286d9f1f8d1ecc1a8e92fbf5335efbd5aeebb45e
Pham Chien <phamchien5870@gmail.com>
2025-03-02 13:01:17 +0700

    update

---
+++ HomeScreen.js
@@ -0,0 +1,16 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Chào mừng bạn đến HomeScreen!</Text>
+      <Button title="Quay lại Đăng nhập" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: { fontSize: 20, fontWeight: 'bold', marginBottom: 20 },
+});
+
